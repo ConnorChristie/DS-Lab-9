2d62efc42e854ec9220aa592be2618f39f0426a8
Connor Christie <christieck@msoe.edu>
2016-05-21 10:16:42 -0500

    Initial commit

---
+++ src/lab9/christieck/Simulator.java
@@ -0,0 +1,16 @@
+package lab9.christieck;
+
+import javax.swing.*;
+
+public class Simulator extends JFrame
+{
+    public Simulator()
+    {
+        
+    }
+
+    public static void main(String[] args)
+    {
+        new Simulator().setVisible(true);
+    }
+}
